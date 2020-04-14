ecdab8f8bd78e52c308a05da9fcfadecd744356e
Alex Harries <alex@greyhead.co.uk>
2020-04-15 00:19:35 +0100

    Tweak doc comments in pingrebooter.sh
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- pingrebooter.sh
+++ pingrebooter.sh
@@ -8,14 +8,14 @@
 #
 # - Every six seconds, it pings www.google.co.uk
 #
-# - If that ping comes back within 30 seconds, the file
+# - If that ping comes back within 10 seconds, the file
 #   /tmp/pingrebooter/failedpingcount.txt is set to 0
 #
-# - If that ping doesn't come back within 30 seconds, the file
+# - If that ping doesn't come back within 10 seconds, the file
 #   /tmp/pingrebooter/failedpingcount.txt is read and its value incremented by
 #   1 before being written back to the file.
 #
-#   So, if the last ping doesn't come back within 30 seconds, and
+#   So, if the last ping doesn't come back within 10 seconds, and
 #   /tmp/pingrebooter/failedpingcount.txt contains "4", then we update the file
 #   with the number 5.
 #
@@ -23,12 +23,12 @@
 #   reboot - load /tmp/pingrebooter/lastreboottime.txt and parse the timestamp
 #   in the file.
 #
-# - If the timestamp is greater than 10 minutes ago, reset
+# - If the timestamp is greater than 15 minutes ago, reset
 #   /tmp/pingrebooter/failedpingcount.txt to 0, set
 #   /tmp/pingrebooter/lastreboottime.txt to now, turn off the relay for 20
 #   seconds, then turn it back on.
 #
-# - If the last reboot timestamp is LESS than 10 minutes ago, we don't want to
+# - If the last reboot timestamp is LESS than 15 minutes ago, we don't want to
 #   reboot yet (because it's likely the last reboot didn't fix the issue; it
 #   might not be a problem that can be fixed by a reboot), so just increment
 #   /tmp/pingrebooter/failedpingcount.txt by 1 and continue.
